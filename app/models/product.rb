1c3a0df7a9a7697461ff4f26c810810b8cff0bdc
Patryk <patrykgarlinski@gmai.com>
2020-03-18 11:08:01 -0600

    B2: Add unit tests using fixtures

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   # validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
-  # validates :image_url, allow_blank: true,
-  # format: {with: %r{\.(gif|jpg|png)\z}i, message: 'must be a JPG, PNG, or GIF'}
+  validates :image_url, allow_blank: true,
+  format: {with: %r{\.(gif|jpg|png)\z}i, message: 'must be a JPG, PNG, or GIF'}
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -17,7 +17,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+      post products_url, params: {
+        product: {
+          description: @product.description,
+          image_url: @product.image_url,
+          price: @product.price,
+          title: @title
+          }
+        }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -34,7 +41,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    patch product_url(@product), params: {
+      product: { description: @product.description,
+      image_url: @product.image_url,
+      price: @product.price,
+      title: @title
+      }
+    }
     assert_redirected_to product_url(@product)
   end
 
